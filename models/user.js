611ff09102e798b0a03fd17ce15bac2c90324435
vagarioustoast <vagarioustoast@gmail.com>
2019-02-19 14:03:58 -0800

    updated models songs reference

--- models/user.js
+++ models/user.js
@@ -4,10 +4,12 @@ Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: String,
   password: String,
-  songs: {
-    type: Schema.Types.ObjectId,
-    ref: "Song"
-  }
+  songs: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Song"
+    }
+  ]
 });
 
 const User = mongoose.model("User", UserSchema);
