93c6b3469f09b063eb5e776d46125a62892f6852
meltedopen <isaac.chang5@gmail.com>
2019-02-19 14:10:38 -0800

    HTML + CSS

---
+++ public/scripts/main.js
@@ -0,0 +1,36 @@
+// Preloader
+$(window).on("load", function() {
+    $(".loader .inner").fadeOut(500, function() {
+        $(".loader").fadeOut(750);
+    });
+});
+
+$(document).ready(function() {
+
+$('#vmap').vectorMap({
+    map: 'usa_en',
+    backgroundColor: '#a5bfdd',
+    borderColor: '#818181',
+    borderOpacity: 0.25,
+    borderWidth: 1,
+    color: '#f4f3f0',
+    enableZoom: true,
+    hoverColor: '#c9dfaf',
+    hoverOpacity: null,
+    normalizeFunction: 'linear',
+    scaleColors: ['#b6d6ff', '#005ace'],
+    selectedColor: '#c9dfaf',
+    selectedRegions: null,
+    showTooltip: true,
+    onRegionClick: function(element, code, region){
+        var message = 'You clicked "'
+            + region
+            + '" which has the code: '
+            + code.toUpperCase();
+
+        alert(message);
+    }
+});
+
+
+});
