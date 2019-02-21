e332a20e846b135d2f6dace44c107a62d9ee8937
meltedopen <isaac.chang5@gmail.com>
2019-02-20 18:10:22 -0800

    added missing bracket

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -40,34 +40,34 @@ $(document).ready(function () {
 
 
     $("#vmap").vectorMap({
-    map: "usa_en",
-    backgroundColor: "#a5bfdd",
-    borderColor: "#818181",
-    borderOpacity: 0.25,
-    borderWidth: 1,
-    color: "#f4f3f0",
-    enableZoom: true,
-    hoverColor: "#c9dfaf",
-    hoverOpacity: null,
-    normalizeFunction: "linear",
-    scaleColors: ["#b6d6ff", "#005ace"],
-    selectedColor: "#c9dfaf",
-    selectedRegions: null,
-    showTooltip: true,
-    onRegionClick: function(element, code, state) {
-      $.ajax({
-        type: "GET",
-        url: `/api/songs/${state}`,
-        data: "json",
-        success: songs => {
-          songs.forEach(song => {
-            $(".songholder").html(
-              `<div>Now Playing: ${song.artist} - ${song.title}</div>`
-            );
-            $("iframe").attr("src", `${song.songUrl}`);
-          });
+        map: "usa_en",
+        backgroundColor: "#a5bfdd",
+        borderColor: "#818181",
+        borderOpacity: 0.25,
+        borderWidth: 1,
+        color: "#f4f3f0",
+        enableZoom: true,
+        hoverColor: "#c9dfaf",
+        hoverOpacity: null,
+        normalizeFunction: "linear",
+        scaleColors: ["#b6d6ff", "#005ace"],
+        selectedColor: "#c9dfaf",
+        selectedRegions: null,
+        showTooltip: true,
+        onRegionClick: function (element, code, state) {
+            $.ajax({
+                type: "GET",
+                url: `/api/songs/${state}`,
+                data: "json",
+                success: songs => {
+                    songs.forEach(song => {
+                        $(".songholder").html(
+                            `<div>Now Playing: ${song.artist} - ${song.title}</div>`
+                        );
+                        $("iframe").attr("src", `${song.songUrl}`);
+                    });
+                }
+            });
         }
-      });
-    }
-  });
-
+    });
+});
